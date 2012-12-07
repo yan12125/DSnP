97a92e964344ff5fd1b3798f323e9c8d2699fbeb
yan12125 <yan12125@gmail.com>
2012-12-08 04:31:12 +0800

    Download hw6

---
+++ hw6/src/cir/cirGate.cpp
@@ -0,0 +1,41 @@
+/****************************************************************************
+  FileName     [ cirGate.cpp ]
+  PackageName  [ cir ]
+  Synopsis     [ Define class CirAigGate member functions ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2008-2012 LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <stdarg.h>
+#include <cassert>
+#include "cirGate.h"
+#include "cirMgr.h"
+#include "util.h"
+
+using namespace std;
+
+extern CirMgr *cirMgr;
+
+/**************************************/
+/*   class CirGate member functions   */
+/**************************************/
+void
+CirGate::reportGate() const
+{
+}
+
+void
+CirGate::reportFanin(int level) const
+{
+   assert (level >= 0);
+}
+
+void
+CirGate::reportFanout(int level) const
+{
+   assert (level >= 0);
+}
+
